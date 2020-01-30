3c40658a4bb2073d94d78b437473ec6e132da99f
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-01-30 15:36:18 -0500

    split GITHUB_REF to get branch/tag name

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e # Abort script at first error
 set -u # nounset - Attempt to use undefined variable outputs error message, and forces an exit
 # set -x # verbose (expands commands)
 
+REF_NAME="$(echo $GITHUB_REF | cut -d '/' -f3)"
 gitleaks -v --exclude-forks --redact --threads=1 \
-  --branch=$GITHUB_SHA \
+  --branch=$REF_NAME \
   --repo-path=$GITHUB_WORKSPACE \
