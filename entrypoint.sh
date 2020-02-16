b94499c02d968614a3760efebd71f96d0c558fcc
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-02-16 03:05:13 -0500

    handle PR commit vs tag name for scan

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,15 @@ set -e # Abort script at first error
 set -u # nounset - Attempt to use undefined variable outputs error message, and forces an exit
 # set -x # verbose (expands commands)
 
-REF_NAME="$(echo $GITHUB_REF | cut -d '/' -f3)"
+if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]] 
+then
+    # last commit on the PR branch
+    REF_NAME=$GITHUB_SHA
+else
+    # tag name in the form "refs/tags/<tag>"
+    REF_NAME="$(echo $GITHUB_REF | cut -d '/' -f3)"
+fi
 gitleaks -v --exclude-forks --redact --threads=1 \
   --branch=$REF_NAME \
   --repo-path=$GITHUB_WORKSPACE \
+
