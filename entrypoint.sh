3547a0ec8747cdfad209711ae1316f8e5eadf4b6
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-02-16 03:23:03 -0500

    use explicit PR commit references

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,16 @@ set -u # nounset - Attempt to use undefined variable outputs error message, and 
 
 if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]] 
 then
-    # last commit on the PR branch
-    REF_NAME=$GITHUB_SHA
+    # from last commit on the PR to source 
+    gitleaks -v --exclude-forks --redact --threads=1 \
+      --commit-from=$GITHUB_SHA \
+      --commit-to=$GITHUB_HEAD_REF \
+      --repo-path=$GITHUB_WORKSPACE \
 else
-    # tag name in the form "refs/tags/<tag>"
+    # tag or branch name in the form "refs/tags/<ref>"
     REF_NAME="$(echo $GITHUB_REF | cut -d '/' -f3)"
+    gitleaks -v --exclude-forks --redact --threads=1 \
+      --branch=$REF_NAME \
+      --repo-path=$GITHUB_WORKSPACE \
 fi
-gitleaks -v --exclude-forks --redact --threads=1 \
-  --branch=$REF_NAME \
-  --repo-path=$GITHUB_WORKSPACE \
 
