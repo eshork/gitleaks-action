edde97c094ae410cf8093403d6441a677caf17dc
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-02-20 14:22:39 -0500

    also don't scan whole history for push master + scope PR scan to new commits

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,27 +13,31 @@ echo "GITHUB_BASE_REF:   ${GITHUB_BASE_REF}"
 cd "$GITHUB_WORKSPACE"
 git fetch --quiet
 
-# branch/tag name in the form "refs/<ref-type>/<ref-id>[/<ref-subtype>]"
-# ref-type: heads|pull|tags
-GITHUB_REF_ID="$(echo $GITHUB_REF | cut -d '/' -f3)"
 if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]] 
 then
-    GITHUB_REF_PULL_TYPE="$(echo $GITHUB_REF | cut -d '/' -f4)"
-    if [[ "${GITHUB_REF_PULL_TYPE}" == "merge" ]]
-    then
-        # during merge, the head ref is the PR branch name
-        GITHUB_REF_ID=${GITHUB_HEAD_REF}
-    fi
     gitleaks -v --exclude-forks --redact --threads=1 \
-      --branch=$GITHUB_REF_ID \
+      --branch=$GITHUB_HEAD_REF \
+      --commit-to=$GITHUB_BASE_REF \
       --repo-path=$GITHUB_WORKSPACE
 else
-    # pushed tag or single commit reference
+    # branch/tag name in the form "refs/<ref-type>/<ref-id>[/<ref-subtype>]"
+    # ref-type: heads|pull|tags
+    GITHUB_REF_NAME="$(echo $GITHUB_REF | cut -d '/' -f3)"
     # run only from current to master instead of full history
-    GITHUB_REF_MASTER=$(git show origin/master --pretty='format:"%H"')
-    gitleaks -v --exclude-forks --redact --threads=1 \
-      --branch=$GITHUB_REF_ID \
-      --commit-to=$GITHUB_REF_MASTER \
-      --repo-path=$GITHUB_WORKSPACE
+    GITHUB_REF_MASTER=$(git show 'origin/master' --pretty='format:%H')
+    
+    if [[ "${GITHUB_REF_NAME}" == "master" ]] 
+    then
+        # push to master (eg: for tagged versioning), only scan the last commit
+        gitleaks -v --exclude-forks --redact --threads=1 \
+          --branch=$GITHUB_REF_NAME \
+          --depth=1 \
+          --repo-path=$GITHUB_WORKSPACE
+    else        
+        gitleaks -v --exclude-forks --redact --threads=1 \
+          --branch=$GITHUB_REF_NAME \
+          --commit-to=$GITHUB_REF_MASTER \
+          --repo-path=$GITHUB_WORKSPACE
+    fi
 fi
 
